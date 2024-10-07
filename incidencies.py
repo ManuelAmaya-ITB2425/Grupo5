3c2120c835136f644cb4c0eed8dcbb2b22f8c68b
manuel.amaya.7e6 <manuel.amaya.7e6@itb.cat>
2024-10-07 09:48:18 +0200

    Prueba

--- incidencies.py
+++ incidencies.py
@@ -1,10 +1,4 @@
 import xml.etree.ElementTree as ET
-tree = ET.parse('Incidencies')
-root = tree.getroot()
 
-root.tag
-
-root.attrib
-
-for child in root:
-    print(child.tag, child.attrib)
+archivo_xml = ET.parse("Incidencies.xml")
+archivo_node = root.
