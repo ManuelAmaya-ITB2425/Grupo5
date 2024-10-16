cc08b0d201401d59dca36221e229b62d43f6bb8d
manuel.amaya.7e6 <manuel.amaya.7e6@itb.cat>
2024-10-16 12:54:16 +0200

    16/10/2024 12:54

--- csv_to_xml.py
+++ csv_to_xml.py
@@ -3,17 +3,19 @@ import pandas as pd
 import re
 
 # Cargar el archivo CSV
-csv_file = 'C:/Users/Usuario/Desktop/incidencias.csv'  # Asegúrate de que este archivo exista
+csv_file = '/home/manuel.amaya.7e6/Baixades/incidencies.csv'  # Asegúrate de que este archivo exista
 df = pd.read_csv(csv_file)
 
 df.columns = df.columns.str.replace(' ', '_')
 
 df.columns = df.columns.str.strip().str.replace(' ', '_').str.replace('\n', '')
 
-df.columns = [re.sub(r'\\W+', '_', col) for col in df.columns]  # Doble barra para escapar
+df.columns = [re.sub(r'\W+', '_', col) for col in df.columns]
 
 # Convertir a XML
-xml_file = 'IncidenciasOk.xml'  # Nombre del archivo XML de salida
+xml_file = 'incidenciasOK.xml'  # Nombre del archivo XML de salida
 df.to_xml(xml_file, index=False)
 
 print(f'Archivo convertido a XML y guardado como: {xml_file}')
+
+# Rodrigo creador
