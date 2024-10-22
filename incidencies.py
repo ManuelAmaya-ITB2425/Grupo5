e16166e040bde8d1f5f3e7f1354dcb130e6750aa
manuel.amaya.7e6 <manuel.amaya.7e6@itb.cat>
2024-10-22 13:01:10 +0200

    22/10/2024 13:01

--- incidencies.py
+++ incidencies.py
@@ -4,20 +4,18 @@ from datetime import datetime
 archivoXML = ET.parse("incidenciasOK.xml") #Per analitzar el arxiu XML
 raiz = archivoXML.getroot() #Per obtenir el node arrel
 
-fechaActual = datetime.now().year
-
 contador = 0
 
 for row in raiz.findall('row'): #Busca tots el elements que coincideixin amb row amb el findall
     marcaTemps = row.find('Marca_de_temps').text #Fa una busqueda de la etiqueta que hem posat i despres
     adreça = row.find('Adreça_electrònica').text
-    informacio = row.find('Informació_relativa_sobre_la_protecció_de_dades_en_compliment_del_Reglament_General_de_Protecció_de_Dades__Reglament_UE_2016_679_del_Parlament_Europeu_i_del_Consell__de_27_d_abril_de_2016_')
-    nomCognom = row.find('NOM_i_COGNOMS')
-    dataIncidencia = row.find('DATA_DE_LA_INCIDÈNCIA')
-    tipusIncidencia = row.find('TIPUS_INCIDÈNCIA')
-    equipsAfectats = row.find('EQUIPS_i_o_SERVEIS_AFECTATS')
-    propostaSolucio = row.find('DESCRIPCIÓ____PROPOSTA_DE_SOLUCIÓ')
-    nivelUrgencia = row.find('NIVELL_URGÈNCIA_DE_SOLUCIÓ')
+    informacio = row.find('Informació_relativa_sobre_la_protecció_de_dades_en_compliment_del_Reglament_General_de_Protecció_de_Dades__Reglament_UE_2016_679_del_Parlament_Europeu_i_del_Consell__de_27_d_abril_de_2016_').text
+    nomCognom = row.find('NOM_i_COGNOMS').text
+    dataIncidencia = row.find('DATA_DE_LA_INCIDÈNCIA').text
+    tipusIncidencia = row.find('TIPUS_INCIDÈNCIA').text
+    equipsAfectats = row.find('EQUIPS_i_o_SERVEIS_AFECTATS').text
+    propostaSolucio = row.find('DESCRIPCIÓ____PROPOSTA_DE_SOLUCIÓ').text
+    nivelUrgencia = row.find('NIVELL_URGÈNCIA_DE_SOLUCIÓ').text
 
     print(f'Marca_de_Temps: {marcaTemps}, '
           f'Adreça_electrònica: {adreça}, '
@@ -30,4 +28,4 @@ for row in raiz.findall('row'): #Busca tots el elements que coincideixin amb row
           f'NIVELL_URGÈNCIA_DE_SOLUCIÓ: {nivelUrgencia}')
 
     contador += 1
-print('\033[0;35m'f'Hay {contador} incidencias')
+print('\033[1;3;4;35m'f'Hay {contador} incidencias')
