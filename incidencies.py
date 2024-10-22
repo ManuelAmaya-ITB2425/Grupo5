e92a677cd2de538d579d66c7e04e9929d6b1c3fb
ManuelAmaya-ITB2425 <manuel.amaya.7e6@itb.cat>
2024-10-22 16:57:18 +0200

    incidencies.py

--- incidencies.py
+++ incidencies.py
@@ -1,13 +1,25 @@
 import xml.etree.ElementTree as ET #ElementTree es una biblioteca en Python que permet analitzar i crear dades XML
 from datetime import datetime
 
-archivoXML = ET.parse("incidenciasOK.xml") #Per analitzar el arxiu XML
-raiz = archivoXML.getroot() #Per obtenir el node arrel
 
-contador = 0
+#Para analizar, cargar y obtener el nodo raíz
+archivoXML = ET.parse("incidenciasOK.xml")
+raiz = archivoXML.getroot()
 
-for row in raiz.findall('row'): #Busca tots el elements que coincideixin amb row amb el findall
-    marcaTemps = row.find('Marca_de_temps').text #Fa una busqueda de la etiqueta que hem posat i despres
+
+#Inicia las variables para contar las variables, tanto las válidas como las que no
+contadorTotal = 0
+contadorBuenas = 0
+contadorMalas = 0
+
+
+#Índica las fechas límites (de 1980 a la fecha actual)
+fechaLimiteInferior = datetime(1980, 1, 1)
+fechaActual = datetime.now()
+
+#Busca cada entrada del elemento row del archivo y extraemos los valores dentro de este, especificando la incidencia
+for row in raiz.findall('row'):
+    marcaTemps = row.find('Marca_de_temps').text
     adreça = row.find('Adreça_electrònica').text
     informacio = row.find('Informació_relativa_sobre_la_protecció_de_dades_en_compliment_del_Reglament_General_de_Protecció_de_Dades__Reglament_UE_2016_679_del_Parlament_Europeu_i_del_Consell__de_27_d_abril_de_2016_').text
     nomCognom = row.find('NOM_i_COGNOMS').text
@@ -17,15 +29,43 @@ for row in raiz.findall('row'): #Busca tots el elements que coincideixin amb row
     propostaSolucio = row.find('DESCRIPCIÓ____PROPOSTA_DE_SOLUCIÓ').text
     nivelUrgencia = row.find('NIVELL_URGÈNCIA_DE_SOLUCIÓ').text
 
-    print(f'Marca_de_Temps: {marcaTemps}, '
-          f'Adreça_electrònica: {adreça}, '
-          f'Informació_relativa_sobre_la_protecció_de_dades_en_compliment_del_Reglament_General_de_Protecció_de_Dades__Reglament_UE_2016_679_del_Parlament_Europeu_i_del_Consell__de_27_d_abril_de_2016_: {informacio},'
-          f'NOM_i_COGNOMS: {nomCognom}, '
-          f'DATA_DE_LA_INCIDÈNCIA: {dataIncidencia}, '
-          f'TIPUS_INCIDÈNCIA: {tipusIncidencia}, '
-          f'EQUIPS_i_o_SERVEIS_AFECTATS: {equipsAfectats},'
-          f'DESCRIPCIÓ____PROPOSTA_DE_SOLUCIÓ: {propostaSolucio},'
-          f'NIVELL_URGÈNCIA_DE_SOLUCIÓ: {nivelUrgencia}')
+    #Verifica si tiene un valor válido
+    if dataIncidencia is not None:
+        try:
+            #Convierte la fecha de la incidencia en un objeto datatime
+            dataIncidenciaDT = datetime.strptime(dataIncidencia, "%d/%m/%Y")
+        except ValueError:
+            #Si hay un error en el formato de la fecha, se considerará incorrecta
+            contadorMalas += 1
+            print(f"Error de format de data: {dataIncidencia} per {nomCognom}")
+            continue
+        #Verifica si la fecha está dentro del rango especificado
+        if fechaLimiteInferior <= dataIncidenciaDT <= fechaActual:
+            contadorBuenas += 1
+        else:
+            contadorMalas += 1
 
-    contador += 1
-print('\033[1;3;4;35m'f'Hay {contador} incidencias')
+    #Muestra la información de las incidencias
+    print(f'Marca_de_Temps:\033[0;31;40m'f'{marcaTemps}\033[0;0m   '
+          f'Adreça_electrònica:\033[0;32;40m'f'{adreça}\033[0;0m   '
+          f'Informació_relativa_sobre_la_protecció_de_dades_en_compliment_del_Reglament_General_de_Protecció_de_Dades__Reglament_UE_2016_679_del_Parlament_Europeu_i_del_Consell__de_27_d_abril_de_2016_:\033[1;3;4;33;40m'f'{informacio}\033[0;0m   '
+          f'NOM_i_COGNOMS:\033[0;34;40m'f'{nomCognom}\033[0;0m   '
+          f'DATA_DE_LA_INCIDÈNCIA:\033[0;35;40m'f'{dataIncidencia}\033[0;0m   '
+          f'TIPUS_INCIDÈNCIA:\033[0;36;40m'f'{tipusIncidencia}\033[0;0m   '
+          f'EQUIPS_i_o_SERVEIS_AFECTATS:\033[0;37;40m'f'{equipsAfectats}\033[0;0m   '
+          f'DESCRIPCIÓ____PROPOSTA_DE_SOLUCIÓ:\033[0;38;40m'f'{propostaSolucio}\033[0;0m   '
+          f'NIVELL_URGÈNCIA_DE_SOLUCIÓ:\033[0;30;47m'f'{nivelUrgencia}\033[0;0m   ')
+
+    contadorTotal += 1
+
+#Calcula los porcentajes
+if contadorTotal > 0:
+    porcentageBuenas = (contadorBuenas / contadorTotal) * 100
+    porcentageMalas = (contadorMalas / contadorTotal) * 100
+
+    #Muestra los resultados
+    print('\033[1;3;4;35m'f"\nTotal de incidencias procesadas:\033[0;35m {contadorTotal}")
+    print('\033[1;3;4;32m'f"Porcentage de incidencias validas:\033[0;32m {porcentageBuenas:.2f}%")
+    print('\033[1;3;4;31m'f"Porcentages de incidencias invalidas:\033[0;31m {porcentageMalas:.2f}%")
+else:
+    print("No se han procesado incidencias")
