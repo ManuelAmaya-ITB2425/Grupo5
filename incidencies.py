97a94acf26f6ce2d59c274e17d566b8f89797edd
manuel.amaya.7e6 <manuel.amaya.7e6@itb.cat>
2024-10-28 09:44:40 +0100

    Casi terminado

--- incidencies.py
+++ incidencies.py
@@ -1,7 +1,7 @@
 import xml.etree.ElementTree as ET #ElementTree es una biblioteca en Python que permet analitzar i crear dades XML
 import json
 from datetime import datetime
-
+import os
 
 #Para analizar, cargar y obtener el nodo raíz
 archivoXML = ET.parse("incidenciasOK.xml")
@@ -13,7 +13,6 @@ contadorTotal = 0
 contadorBuenas = 0
 contadorMalas = 0
 incidencias = []
-salidaMostrar = []
 
 #Índica las fechas límites (de 1980 a la fecha actual)
 fechaLimiteInferior = datetime(1980, 1, 1)
@@ -41,7 +40,7 @@ for row in raiz.findall('row'):
         except ValueError:
             #Si hay un error en el formato de la fecha, se considerará incorrecta
             contadorMalas += 1
-            salidaMostrar.append(f"Error de formato de fecha: {dataIncidencia} para {nomCognom}")
+            incidencias.append(f"Error de formato de fecha: {dataIncidencia} para {nomCognom}")
             continue
         #Verifica si la fecha está dentro del rango especificado
         if fechaLimiteInferior <= dataIncidenciaDT <= fechaActual:
@@ -51,45 +50,41 @@ for row in raiz.findall('row'):
             contadorMalas += 1
             valid = False
 
-    # Guardar la incidencia en la lista
-    infoIncidencias = {
-        "Marca_de_Temps": marcaTemps,
-        "Adreça_electrònica": adreça,
-        "Informació": informacio,
-        "Nom_i_Cognoms": nomCognom,
-        "Data_de_la_Incidència": dataIncidencia,
-        "Tipus_Incidència": tipusIncidencia,
-        "Equips_i_o_Serveis_Afectats": equipsAfectats,
-        "Proposta_Solucio": propostaSolucio,
-        "Nivell_Urgència": nivelUrgencia,
-        "Valida": valid  # Indica si la incidencia es válida o no
-    }
-    incidencias.append(infoIncidencias)
-    salidaMostrar.append(f'Marca_de_Temps:\033[0;31;40m'f'{marcaTemps}\033[0;0m   '
-                         f'Adreça_electrònica:\033[0;32;40m'f'{adreça}\033[0;0m   '
-                         f'Informació_relativa_sobre_la_protecció_de_dades_en_compliment_del_Reglament_General_de_Protecció_de_Dades__Reglament_UE_2016_679_del_Parlament_Europeu_i_del_Consell__de_27_d_abril_de_2016_:\033[1;3;4;33;40m'f'{informacio}\033[0;0m   '
-                         f'NOM_i_COGNOMS:\033[0;34;40m'f'{nomCognom}\033[0;0m   '
-                         f'DATA_DE_LA_INCIDÈNCIA:\033[0;35;40m'f'{dataIncidencia}\033[0;0m   '
-                         f'TIPUS_INCIDÈNCIA:\033[0;36;40m'f'{tipusIncidencia}\033[0;0m   '
-                         f'EQUIPS_i_o_SERVEIS_AFECTATS:\033[0;37;40m'f'{equipsAfectats}\033[0;0m   '
-                         f'DESCRIPCIÓ____PROPOSTA_DE_SOLUCIÓ:\033[0;38;40m'f'{propostaSolucio}\033[0;0m   '
-                         f'NIVELL_URGÈNCIA_DE_SOLUCIÓ:\033[0;30;47m'f'{nivelUrgencia}\033[0;0m   ')
+    print(f'Marca_de_Temps:\033[0;31;40m'f'{marcaTemps}\033[0;0m   '
+          f'Adreça_electrònica:\033[0;32;40m'f'{adreça}\033[0;0m   '
+          f'Informació_relativa_sobre_la_protecció_de_dades_en_compliment_del_Reglament_General_de_Protecció_de_Dades__Reglament_UE_2016_679_del_Parlament_Europeu_i_del_Consell__de_27_d_abril_de_2016_:\033[1;3;4;33;40m'f'{informacio}\033[0;0m   '
+          f'NOM_i_COGNOMS:\033[0;34;40m'f'{nomCognom}\033[0;0m   '
+          f'DATA_DE_LA_INCIDÈNCIA:\033[0;35;40m'f'{dataIncidencia}\033[0;0m   '
+          f'TIPUS_INCIDÈNCIA:\033[0;36;40m'f'{tipusIncidencia}\033[0;0m   '
+          f'EQUIPS_i_o_SERVEIS_AFECTATS:\033[0;37;40m'f'{equipsAfectats}\033[0;0m   '
+          f'DESCRIPCIÓ____PROPOSTA_DE_SOLUCIÓ:\033[0;38;40m'f'{propostaSolucio}\033[0;0m   '
+          f'NIVELL_URGÈNCIA_DE_SOLUCIÓ:\033[0;30;47m'f'{nivelUrgencia}\033[0;0m   ')
+
     contadorTotal += 1
 
 #Calcula los porcentajes
 if contadorTotal > 0:
     porcentageBuenas = (contadorBuenas / contadorTotal) * 100
     porcentageMalas = (contadorMalas / contadorTotal) * 100
-
+    # Guardar la incidencia en la lista
+    infoIncidencias = {
+        "Fecha": f'{fechaActual}',
+        "Total de incidencias procesadas ": contadorTotal,
+        "Total de incidencias validas ": contadorBuenas,
+        "Total de incidencias invalidas ": contadorMalas,
+        "Porcentage de las incidencias validas ": f'{porcentageBuenas:.2f}%',
+        "Porcentage de las incidencias invalidas ": f'{porcentageMalas:.2f}%' # Indica si la incidencia es válida o no
+    }
+    incidencias.append(infoIncidencias)
     #Muestra los resultados
-    salidaMostrar.append('\033[1;3;4;35m'f"\nTotal de incidencias procesadas:\033[0;35m {contadorTotal}")
-    salidaMostrar.append('\033[1;3;4;32m'f"Porcentage de incidencias validas:\033[0;32m {porcentageBuenas:.2f}%")
-    salidaMostrar.append('\033[1;3;4;31m'f"Porcentages de incidencias invalidas:\033[0;31m {porcentageMalas:.2f}%")
+    print('\033[1;3;4;35m'f"\nTotal de incidencias procesadas:\033[0;35m {contadorTotal}")
+    print('\033[1;3;4;32m'f"Porcentage de incidencias validas:\033[0;32m {porcentageBuenas:.2f}%")
+    print('\033[1;3;4;31m'f"Porcentages de incidencias invalidas:\033[0;31m {porcentageMalas:.2f}%")
 else:
-    salidaMostrar.append("No se han procesado incidencias")
+    print('\033[0;31m'f"No se han procesado incidencias\033[0;31m")
 
 ficheroJSON = "incidencias.json"
 with open(ficheroJSON, 'w', encoding='utf-8') as json_file:
-    json.dump(salidaMostrar, json_file, ensure_ascii=False, indent=4)
+    json.dump(infoIncidencias, json_file, ensure_ascii=False, indent=4)
 
-print(f"\nEl fichero JSON '{ficheroJSON}' se ha creado exitosamente en la misma ubicacion que el script")
+print('\033[0;34m'f"\nEl fichero JSON '{ficheroJSON}' se ha creado exitosamente en la misma ubicacion que el script\033[0;34m")
